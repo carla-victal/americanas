b668a3b922535e5f2ba7eea3f08fe8c481f64732
Carla dos Santos Victal <carla.victal@mrv.com.br>
2020-08-04 19:58:26 -0300

    cucumber yaml

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,32 @@ require "capybara"
 require "capybara/cucumber"
 require "selenium-webdriver"
 
+World Capybara::DSL
+
+CONFIG = YAML.load_file(File.join(Dir.pwd, "features/support/config/#{ENV["ENV_TYPE"]}.yaml"))
+
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid browser"
+end
+
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
-  #config.app_host = "https://www.americanas.com.br/"
+  config.default_driver = @driver
+  config.app_host = CONFIG["url"]
   config.default_max_wait_time = 10
 end
